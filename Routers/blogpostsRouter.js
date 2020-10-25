af7848ecfd434960bb12b39aed37a06095bd997a
David Acosta <acosta816@gmail.com>
2020-10-25 11:11:11 -0700

    added get/ to blogpostsRouter

--- Routers/blogpostsRouter.js
+++ Routers/blogpostsRouter.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-const { BlogPosts } = require('../models');
+const { BlogPost } = require('../models');
 
 
 //add volitile data to view and work with.
-const blogs = [
-    { title: "How to express app", content: "blahhhh balhhhhh", author: { firstName: "Bonk", lastName: "The Troll" } },
-    { title: "Bee and Puppycat moves to Netflix!", content: "The awesome show is now on Netflix! YEAHH het a GUM!!", author: "Bee the spaceperson" },
-    { title: "Funnylying", content: "It's lying but they KNOW it's lying so it's funny. Funny lying...hehe get it?..", author: "Puppycat/Space Outlaw" }
-]
+// const blogs = [
+//     { title: "How to express app", content: "blahhhh balhhhhh", author: { firstName: "Bonk", lastName: "The Troll" } },
+//     { title: "Bee and Puppycat moves to Netflix!", content: "The awesome show is now on Netflix! YEAHH het a GUM!!", author: "Bee the spaceperson" },
+//     { title: "Funnylying", content: "It's lying but they KNOW it's lying so it's funny. Funny lying...hehe get it?..", author: "Puppycat/Space Outlaw" }
+// ]
 //declaring a function that maps over blogs array to make some volitile data
-const makeSomePosts = (blogs) => blogs.map(b => BlogPosts.create(b.title, b.content, b.author));
+// const makeSomePosts = (blogs) => blogs.map(b => BlogPosts.create(b.title, b.content, b.author));
 
-makeSomePosts(blogs);//calling function we made.
+// makeSomePosts(blogs);//calling function we made.
 
 router.get('/', (req, res) => {
-    res.status(200).json(BlogPosts.get());
-});
+    const filters = {};
+    const queryableFields = ['title', 'author'];
+    queryableFields.forEach(field => {
+        if (req.query[field]) {
+            filters[field] = req.query[field];
+        }
+    });//end of queryableFields.forEach()
+    BlogPost.find(filters)
+        .then(blogs => {
+            res.json({
+                count: blogs.length,
+                blogs: blogs.map(blog => blog.easyRead())
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+        });
+});//end of GET/
 
 router.post('/', (req, res) => {
     console.log(req.body);
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 //imports
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+
+//mongoose internally uses a "promise-like" object,
+//but it's better to have Mongoose use built-in es6 promises.
+mongoose.Promise = global.Promise;
+
+//import constants for env vars
+const { PORT, DATABASE_URL } = require('./config');
 
 //initailize express app
 const app = express();
@@ -22,41 +30,46 @@ app.use('/blog-posts', blogpostsRouter);
 
 let server;
 
-const runServer = () => {
-
-    const port = process.env.PORT || 8080;
-
+const runServer = (databaseUrl, port = PORT) => {
     return new Promise((resolve, reject) => {
-        server = app
-            .listen(port, () => {
-                console.log(`Your app is listening on port ${port}`);
-                resolve(server);
-            })
-            .on('error', err => {
-                console.log(`Something went arry, here's the error: ${err}`)
-                reject(err);
-            });
-    });// end of Promise
-};
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app
+                .listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve(server);
+                })
+                .on('error', err => {
+                    console.log(`Something went arry, here's the error: ${err}`)
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        })//end of mongoose.connect()
+    });// end of new Promise
+};//end of runServer()
 
 
 const closeServer = () => {
-    return new Promise((resolve, reject) => {
-        console.log('CLOSING SERVER');
-        server.close(err => {
-            if (err) {
-                console.log('Error with closing the server, here is the error:', err);
-                reject(err);
-                return;
-            };
-            resolve(); //if no err, resolve
-        });
-    });//end of new Promise
-};
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('CLOSING SERVER');
+            server.close(err => {
+                if (err) {
+                    console.log('Error with closing the server, here is the error:', err);
+                    reject(err);
+                    return;
+                };
+                resolve(); //if no err, resolve
+            });
+        });//end of new Promise
+    })//end of .then()
+};//end of closeServer()
 
 //will only run by itslef runServer() if it's being called normally. but NOT if it's being called from a test.
 if (require.main === module) {
-    runServer()
+    runServer(DATABASE_URL)
         .catch(err => console.error(err));
 }
 
