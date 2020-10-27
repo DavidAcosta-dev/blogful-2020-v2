dcc15007ef36b833630b5b361cbc5159e279e27a
David Acosta <acosta816@gmail.com>
2020-10-27 16:38:51 -0700

    integrated mongoose into all CRUD operations

--- Routers/blogpostsRouter.js
+++ Routers/blogpostsRouter.js
@@ -3,23 +3,9 @@ const router = express.Router();
 
 const { BlogPost } = require('../models');
 
-
-//add volitile data to view and work with.
-// const blogs = [
-//     { title: "How to express app", content: "blahhhh balhhhhh", author: { firstName: "Bonk", lastName: "The Troll" } },
-//     { title: "Bee and Puppycat moves to Netflix!", content: "The awesome show is now on Netflix! YEAHH het a GUM!!", author: "Bee the spaceperson" },
-//     { title: "Funnylying", content: "It's lying but they KNOW it's lying so it's funny. Funny lying...hehe get it?..", author: "Puppycat/Space Outlaw" }
-// ]
-//declaring a function that maps over blogs array to make some volitile data
-// const makeSomePosts = (blogs) => blogs.map(b => BlogPosts.create(b.title, b.content, b.author));
-
-// makeSomePosts(blogs);//calling function we made.
-
 router.get('/', (req, res) => {
     const filters = {};
     const queryableFields = ['title', 'author'];
-    console.log("YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO query below")
-    console.log(req.query)
     queryableFields.forEach(field => {
         if (req.query[field]) {
             filters[field] = req.query[field];
@@ -38,6 +24,19 @@ router.get('/', (req, res) => {
         });
 });//end of GET/
 
+
+//GET by ID using mongoose's convinience method for finding by id
+router.get('/:id', (req, res) => {
+    BlogPost
+        .findById(req.params.id)
+        .then(blog => res.json(blog.easyRead()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+        });
+});
+
+
 router.post('/', (req, res) => {
     console.log(req.body);
     const requiredFields = ['title', 'content', 'author'];
@@ -47,40 +46,49 @@ router.post('/', (req, res) => {
             missingFields.push(requiredFields[i])
             console.log(`missing a field: ${requiredFields[i]}`);
         }
-    }
-
-    missingFields.length > 0 ? res.status(400).send(`Please check your Blog post object. Missing following fields: ${missingFields}`) : null;
-    const { title, author, content } = req.body;
-    res.status(201).json(BlogPosts.create(title, content, author));
-});// end of post/
-
-router.put('/:id', (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    const requiredFields = ["title", "content", "author", "id"];
-    const missingFields = [];
-
-    if (req.params.id !== req.body.id) {
-        res.status(400).send(`Please check that you have entered a valid blogpost id in the url OR that both the updating object and url ids match.`);
-    }
-
-    for (i = 0; i < requiredFields.length; i++) {
-        if (!Object.keys(req.body).includes(requiredFields[i])) {
-            missingFields.push(requiredFields[i])
-            console.log(`missing a field: ${requiredFields[i]}`);
-        }
     };
     missingFields.length > 0 ? res.status(400).send(`Please check your Blog post object. Missing following fields: ${missingFields}`) : null;
+    const { title, author, content } = req.body;
+    BlogPost.create({
+        title,
+        content,
+        author
+    })
+        .then(blog => res.status(201).json(blog.easyRead()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+        });
+});// end of post/
 
 
-    res.status(200).json(BlogPosts.update(req.body));
+router.put('/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message =
+            `Request path id (${req.params.id}) and request body id ` +
+            `(${req.body.id}) must match`;
+        console.error(message);
+        return res.status(400).json({ message: message });
+    }
+    const toUpdate = {};
+    const updateableFields = ["title", "author", "content"];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            toUpdate[field] = req.body[field];
+        }
+    });
+    BlogPost
+        .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+        .then(blog => res.status(204).end())
+        .catch(err => res.status(500).json({ message: "Internal server error" }));
 });//end of put
+
 
 router.delete('/:id', (req, res) => {
     console.log(req.params.id);
-    BlogPosts.delete(req.params.id);
-    console.log(`DELETED blog post with id of ----> ${req.params.id}`);
-    res.status(204).end();
+    BlogPost.findByIdAndDelete(req.params.id)
+        .then(blog => res.status(204).end())
+        .catch(err => res.status(500).json({ message: "Internal server error" }));
 })//end of delete
 
 
